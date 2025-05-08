c4c128b64435f5a372351a453be1a56b0640801b
Simone Puliti <>
2025-05-08 18:37:37 +0200

    dark menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -35,7 +35,7 @@ const Navigation = () => {
 
   return ( 
     <nav className="bg-white shadow-sm sticky top-0 z-50 transition-transform duration-300 dark:bg-gray-800">
-      <div className="container mx-auto px-4">
+      <div className="container w-full lg:max-w-screen-lg mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
             <span className="text-base md:text-xl font-bold tracking-widest">
@@ -51,7 +51,7 @@ const Navigation = () => {
           </div>
 
           {/* Menu Desktop */}
-          <div className="hidden md:flex space-x-8 items-center">
+          <div className="hidden lg:flex space-x-8 items-center">
             <a href="#perche" onClick={handleScroll} className="text-base text-gray-600 hover:text-indigo-600 transition-colors duration-200 dark:text-white">
               Perché Ribilanciare
             </a>
@@ -72,7 +72,7 @@ const Navigation = () => {
 
           {/* Hamburger Menu Button */}
           <button 
-            className="md:hidden p-2 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="lg:hidden p-2 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             <svg 
@@ -98,7 +98,7 @@ const Navigation = () => {
               )}
             </svg>
           </button>
-          <button onClick={() => setDarkMode(prev => !prev)}>
+          <button className="hidden lg:flex ml-4" onClick={() => setDarkMode(prev => !prev)}>
       {darkMode ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 dark:stroke-cyan-500">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
 </svg>
@@ -110,7 +110,19 @@ const Navigation = () => {
         </div>
 
         {/* Mobile Menu */}
-        <div className={`md:hidden transition-all duration-300 ease-in-out ${isMenuOpen ? 'max-h-48 opacity-100' : 'max-h-0 opacity-0'} overflow-hidden`}>
+        <div className={`lg:hidden transition-all duration-300 ease-in-out ${isMenuOpen ? 'max-h-full opacity-100' : 'max-h-0 opacity-0'} overflow-hidden`}>
+        <div className="px-4 py-1 mb-2">
+            <button className="" onClick={() => setDarkMode(prev => !prev)}>
+      {darkMode ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 dark:stroke-cyan-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+</svg>
+ : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+</svg>
+}
+    </button>
+            </div>
+          
           <div className="py-2 space-y-1">
             <a 
               href="#perche" 
@@ -142,6 +154,7 @@ const Navigation = () => {
                 Calcolatore
               </a>
             </div>
+            
           </div>
         </div>
       </div>
