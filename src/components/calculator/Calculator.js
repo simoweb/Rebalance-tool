1f90f53fcce60ebfb4067eb7ad5f89052b729c20
Simone Puliti <>
2025-06-04 23:03:38 +0200

    fix label mobile

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -293,8 +293,8 @@ const Calculator = () => {
 
                                 {(rebalanceMethod === 'add' || rebalanceMethod === 'add_and_rebalance') && (
                                     <div className="mb-6">
-                                        <div className="flex items-center">
-                                            <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">
+                                        <div className="c">
+                                            <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">
                                                 Liquidità (€)
                                             </label>
                                             <input
@@ -326,28 +326,28 @@ const Calculator = () => {
                                             )}
                                             <div className="space-y-4">
                                                 {/* Input fields (Nome, Target, Prezzo, Quantità) come prima */}
-                                                <div className="flex items-center">
-                                                    <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Nome</label>
+                                                <div className="flex flex-col md:flex-row">
+                                                    <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Nome</label>
                                                     <input type="text" placeholder="es: VWCE" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent" value={asset.name} onChange={(e) => updateAsset(index, 'name', e.target.value)} />
                                                 </div>
-                                                <div className="flex items-center">
-                                                    <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Target (%)</label>
+                                                <div className="flex flex-col md:flex-row">
+                                                    <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Target (%)</label>
                                                     <input type="text" inputMode="decimal" placeholder="es: 60 o 60,5" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent" value={asset.targetPercentage} onChange={(e) => updateAsset(index, 'targetPercentage', e.target.value)} />
                                                 </div>
-                                                <div className="flex items-center">
-                                                    <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Prezzo (€)</label>
+                                                <div className="flex flex-col md:flex-row">
+                                                    <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Prezzo (€)</label>
                                                     <input type="text" inputMode="decimal" placeholder="es: 100 o 100,25" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent" value={asset.currentPrice} onChange={(e) => updateAsset(index, 'currentPrice', e.target.value)} />
                                                 </div>
-                                                <div className="flex items-center">
-                                                    <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">PMC</label>
+                                                <div className="flex flex-col md:flex-row">
+                                                    <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">PMC</label>
                                                     <input type="text" inputMode="decimal" placeholder="es: 100 o 100,25" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent" value={asset.pmc} onChange={(e) => updateAsset(index, 'pmc', e.target.value)} />
                                                 </div>
-                                                <div className="flex items-center">
-                                                    <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Quantità</label>
+                                                <div className="flex flex-col md:flex-row">
+                                                    <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Quantità</label>
                                                     <input type="text" inputMode="decimal" placeholder="es: 10 o 10,3" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent" value={asset.quantity} onChange={(e) => updateAsset(index, 'quantity', e.target.value)} />
                                                 </div>
-                                                <div className="flex items-center">
-                                                    <label className="w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Tax rate</label>
+                                                <div className="flex flex-col md:flex-row">
+                                                    <label className="w-full md:w-1/3 text-sm md:text-base font-medium text-gray-700 dark:text-gray-400">Tax rate</label>
                                                     <input type="text" inputMode="decimal" placeholder="es: 26%, 12,5%" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent" value={asset.taxRate} onChange={(e) => updateAsset(index, 'taxRate', e.target.value)} />
                                                 </div>
 
