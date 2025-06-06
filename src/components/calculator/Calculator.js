f376491983e4718f11b66e96d4d252be7b6abe5a
Simone Puliti <>
2025-06-06 10:36:45 +0200

    clear form button

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -537,6 +537,14 @@ const Calculator = () => {
                                                     "Completa tutti i campi degli asset. Assicurati che la somma delle percentuali target sia 100% e, se necessario, inserisci la liquidità."
                                                 }</p>
                                             </div>
+                                            {isCurrentDataComplete()  ? "" :  (
+                                                assets.length > 0 && assets.some(a => String(a.targetPercentage).trim() !== '') && Math.abs(getTotalPercentage() - 100) >= 0.01 &&
+                                            
+                                            <div className="relative mt-4">
+                                                    <button onClick={() => setShowClearConfirm(true)} className="flex-1 flex items-center justify-center px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors"><svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>Pulisci Form</button>
+                                                </div>
+                                            )
+                                            }
 
                                             {isCurrentDataComplete() ? "" : (assets.length > 0 && assets.some(a => String(a.targetPercentage).trim() !== '') && Math.abs(getTotalPercentage() - 100) >= 0.01) ?
                                                 "" :
