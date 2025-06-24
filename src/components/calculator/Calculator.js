d9baaa9d709f2b8636583aad69c8ebaee77e78e7
Simone Puliti <>
2025-06-24 16:01:25 +0200

    form style

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -265,7 +265,7 @@ const Calculator = () => {
                                             Metodo
                                         </label>
                                         <select
-                                            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-400 dark:border-transparent"
+                                            className="select-form"
                                             value={rebalanceMethod}
                                             onChange={handleMethodChange}
                                         >
